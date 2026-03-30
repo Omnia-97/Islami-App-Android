05600822de149868d17ffc9d7e3ea1200492fc0c
Omnia Nabil <omniaanabil11920@gmail.com>
2026-03-30 12:26:39 +0200

    bottom navigation

---
+++ app/src/main/java/com/example/islamiapp/ui/screens/main/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.islamiapp.ui.screens.main
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.islamiapp.R
+import com.example.islamiapp.ui.screens.main.fragments.hadeth.HadethFragment
+import com.example.islamiapp.ui.screens.main.fragments.quran.QuranFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigation: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, QuranFragment())
+            .commit()
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.quranTab -> {
+                    showFragment(QuranFragment())
+                }
+
+                R.id.hadethTab -> {
+                    showFragment(HadethFragment())
+                }
+
+                R.id.sebhaTab -> {
+                    Toast.makeText(this, "Task", Toast.LENGTH_SHORT).show()
+                    return@setOnItemSelectedListener false
+                }
+
+                R.id.radioTab -> {
+                    Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show()
+                    return@setOnItemSelectedListener false
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
